cb1d6eab6aeba8424e5463624630368358768472
Durga Prasad <durgaprasad.b@nyros.in>
2011-10-04 16:29:46 +0530

    modified items controller products method .

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 	# This for  supply data to google table .
 	def products
 	    @products = Product.all
+	    #~ This code convert prodcuts result array convertt to json object data
 	    #~ @aa = Product.all(:select => ['name','descrtiption','price'])
 	    #~ puts @aa.to_json
 	end	
