c4c54891a2f98826531e07840020f4a7991fce0d
Durga Prasad <bdurgaprasad_nyros@yahoo.com>
2011-10-04 16:26:17 +0530

    modified items controller .

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-	
+	# This for  supply data to google table .
 	def products
 	    @products = Product.all
-	    @aa = Product.all(:select => ['name','descrtiption','price'])
-	    puts @aa.to_json
+	    #~ @aa = Product.all(:select => ['name','descrtiption','price'])
+	    #~ puts @aa.to_json
 	end	
 end
